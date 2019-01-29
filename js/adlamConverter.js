5163fac903e9f780914739c054237254ff303a44
Craig Cornelius <ccornelius@google.com>
2019-01-29 13:08:48 -0800

    More updates to conversion output

--- js/adlamConverter.js
+++ js/adlamConverter.js
@@ -6,12 +6,17 @@ function combineMaps() {
   var key_arab = adlam_convert_unicode_map;
   for (k in key_arab) {
     var stripped_key = k;
-    stripped_key = k.replace(/\s/g,'')
+    if (k !== " ") {
+      stripped_key = k.replace(/\s/g,'')
+    }
     private_use_map_combined[stripped_key] = [adlam_convert_unicode_map[k], ''];
   }
 
   for (k in adlam_Latin_to_unicode_map) {
-    stripped_key = k.replace(/\s/g,'')
+    stripped_key = k;
+    if (k !== " ") {
+      stripped_key = k.replace(/\s/g,'')
+    }
     if (private_use_map_combined[stripped_key]) {
       private_use_map_combined[stripped_key] =
 	  [adlam_convert_unicode_map[k], adlam_Latin_to_unicode_map[k]];
